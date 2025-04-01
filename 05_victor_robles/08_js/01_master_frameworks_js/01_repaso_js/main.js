481b4c0ebfe44ee6237f7391340efb68af26d404
José Antonio Márquez <jolejuma.sspa@gmail.com>
2025-04-02 01:05:16 +0200

    01:05 horas

---
+++ 05_victor_robles/08_js/01_master_frameworks_js/01_repaso_js/main.js
@@ -0,0 +1,16 @@
+//alert('Hola mundo con javascript!!');
+var nombre = 'José Antonio';
+var altura = 167;
+
+var concatenacion = nombre +" "+ altura;
+// Escribir en el documento
+document.writeln("Nombre: " + nombre); // Esto escribirá el nombre en el documento
+document.writeln("<br>"); // Salto de línea
+document.writeln("Altura: " + altura); // Esto escribirá la altura en el documento
+document.writeln("<br>"); // Salto de línea
+
+document.writeln(concatenacion);
+
+
+//document.writeln(nombre);
+//document.writeln(altura);no  funciona. no aparece en pantalla el nombre y la altura.
