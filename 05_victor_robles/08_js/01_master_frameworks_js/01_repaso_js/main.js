10fdb71199a68f2cd714e1e558d644d2bd9c894f
José Antonio Márquez <jolejuma.sspa@gmail.com>
2025-04-02 16:45:17 +0200

    16:45

--- 05_victor_robles/08_js/01_master_frameworks_js/01_repaso_js/main.js
+++ 05_victor_robles/08_js/01_master_frameworks_js/01_repaso_js/main.js
@@ -1,16 +1,52 @@
-//alert('Hola mundo con javascript!!');
-var nombre = 'José Antonio';
-var altura = 167;
+/*var nombre = "José Antonio";
+var altura = "172";
 
-var concatenacion = nombre +" "+ altura;
-// Escribir en el documento
-document.writeln("Nombre: " + nombre); // Esto escribirá el nombre en el documento
-document.writeln("<br>"); // Salto de línea
-document.writeln("Altura: " + altura); // Esto escribirá la altura en el documento
-document.writeln("<br>"); // Salto de línea
+var concatenacion = nombre + " " + altura;
 
-document.writeln(concatenacion);
+var datos = document.getElementById("datos");
+datos.innerHTML = `
+<h1>Soy la caja de datos</h1>    
+<h2>Mi  nombre es :${nombre}<h2>
+<h3>Mido: ${altura} cm</h3>
+`;
+if(altura <=170){
+    datos.innerHTML += `<h1>    Eres bajito.    </h1>    `;
+}else{
+    datos.innerHTML += `<h1>    Eres más alto de lo esperado.    </h1>    `;
+}
 
+for(var i = 10;i<=25; i++){
+    //bloque de instrucciones
+    datos.innerHTML += "<h2>Estamos en el año: "+i;
+}
+    */
 
-//document.writeln(nombre);
-//document.writeln(altura);no  funciona. no aparece en pantalla el nombre y la altura.
+function MuestraMiNombre(nombre, altura){
+    var misDatos =  `
+<h1>Soy la caja de datos</h1>    
+<h2>Mi  nombre es :${nombre}<h2>
+<h3>Mido: ${altura} cm</h3>
+`;
+return misDatos;
+}
+function imprimir(){
+    var datos = document.getElementById("datos");
+    datos.innerHTML = MuestraMiNombre("José Antonio Márquez", 167);
+}
+imprimir();
+
+var nombres = ["Victor", "José", "Pablo"]
+document.writeln('<h1>Listado de Nombres</h1>');
+/*
+for(i = 0; i < nombres.length; i++){
+    document.writeln(nombres[i] + "<br>");
+}
+    */
+   /*
+   nombres.forEach(function(nombre){
+    document.writeln(nombre +  "<br>");   
+   });
+   */
+   nombres.forEach((nombre) => {
+    document.writeln(nombre +  "<br>");   
+   });
