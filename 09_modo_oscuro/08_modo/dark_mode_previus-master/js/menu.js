5f942f28f899f77af14cf72889f4a90a77544cdf
José Antonio Márquez <jolejuma.sspa@gmail.com>
2025-04-16 11:46:52 +0200

    11:46

--- 09_modo_oscuro/08_modo/dark_mode_previus-master/js/menu.js
+++ 09_modo_oscuro/08_modo/dark_mode_previus-master/js/menu.js
@@ -1,5 +1,5 @@
 const btn_menu = document.querySelector(".btn-menu"),
-      menu_options = document.querySelector(".menu-options");
+    menu_options = document.querySelector(".menu-options");
 
 btn_menu.onclick = () => {
     menu_options.classList.toggle("active");
