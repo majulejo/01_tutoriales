2c26a57c2aba6ca6672ee1bd1f14a8b606c572e0
José Antonio Márquez <jolejuma.sspa@gmail.com>
2025-09-02 19:18:44 +0200

    hh

--- 14_agencia_viajes/adventurero-static-02_pagina_tours/js/tours.js
+++ 14_agencia_viajes/adventurero-static-02_pagina_tours/js/tours.js
@@ -1,23 +1,24 @@
-const filterSelect = document.querySelector('#filter-select');
-const containerFilters = document.querySelectorAll('.container-filter');
+const filterSelect = document.querySelector("#filter-select");
+const containerFilters = document.querySelectorAll(".container-filter");
 
-filterSelect.addEventListener('change', e => {
-    const selectedValue = e.target.value;
+filterSelect.addEventListener("change", (e) => {
+  const selectedValue = e.target.value;
 
-    containerFilters.forEach(containerFilter => containerFilter.classList.remove('active')
-);
+  containerFilters.forEach((containerFilter) =>
+    containerFilter.classList.remove("active")
+  );
 
-    if (selectedValue === 'all') {
-        containerFilters.forEach(containerFilter => containerFilter.classList.add('active')
+  if (selectedValue === "all") {
+    containerFilters.forEach((containerFilter) =>
+      containerFilter.classList.add("active")
     );
-    }
+  }
 
-    const matchingFilter = document.querySelector(`.container-filter[data-filter="${selectedValue}"]`
-        
-    );
+  const matchingFilter = document.querySelector(
+    `.container-filter[data-filter="${selectedValue}"]`
+  );
 
-    if (matchingFilter) {
-        matchingFilter.classList.add('active');
-    }
-
+  if (matchingFilter) {
+    matchingFilter.classList.add("active");
+  }
 });
