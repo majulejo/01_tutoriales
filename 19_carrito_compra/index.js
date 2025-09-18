784aa41d50a1186863af7dbb84204412a13b651d
José Antonio Márquez <jolejuma.sspa@gmail.com>
2025-09-19 00:37:18 +0200

    zz

--- 19_carrito_compra/index.js
+++ 19_carrito_compra/index.js
@@ -50,13 +50,134 @@ function displayProductos(productos) {
 
 /*Función para añadir productos al carrrito*/
 
+function addToCart(productId, buttonElement) {
+    const cantidadElement = document.getElementById(`cantidad-${productId}`);
+    const quantity = parseInt(cantidadElement.textContent, 10);
+
+    const existingProduct = cart.find(item => item.id === productoId);
+
+    if (existingProduct) {
+        existingProduct.quantity += quantity;
+    } else {
+        fetch('productos.json')
+        .then(response => response.json())
+        .then(productos => {
+            const product = productos.find(p => p.id === productId);
+            if (product) {
+                cart,push({...product, quantity: quantity});
+                displayCart();
+            }
+        })
+        .catch(error => console.error('Error al obtener el producto', error));
+    }
+
+    buttonElement.style.display = 'none';
+
+    const cantidadContainer = buttonElement.nextElementSibling;
+    cantidadContainer.style.display = ('flex');
+
+    const cardImgElement = buttonElement.closest('.cards__card').querySelector('.card__img');
+    cardImgElement.clasList.add('active');
+
+}
+
 /*Función para incrementar la cantidad de un producto*/
 
-/*Función para disminuir la cantidad de un producto*/
+function incrementarProducto(productId) {
+    const cantidadElemento = document.getElementById(`cantidad-${productId}`);
+    let cantidad = parseInt(cantidadElemento.textContent, 10);
+    cantidad++;
+    cantidadElemento.textContent = cantidad;
+
+    const producto = cart.find(item => item.id === productId);
+    if (producto) {
+        producto.quantity = cantidad;
+    }
+
+    displayCart();
+}
+
+/*Función para decrementar la cantidad de un producto*/
+
+function decrementarProducto(productId) {
+    const cantidadElemento = document.getElementById(`cantidad-${productId}`);
+    let cantidad = parseInt(cantidadElemento.textContent, 10);
+
+    if (cantidad > 1) {
+        cantidad--;
+        cantidadElemento.textContent = cantidad;
+    } else {
+        cantidad = 0;
+
+        cart = cart.filter(item => item.id !== productId);
+
+        const cantidadContainer = cantidadElemento.parentElement;
+        const buttonElement = cantidadContainer.previusElementSibling;
+        cantidadContainer.style.display = 'none';
+        buttonElement.style.display = 'inline-block';
+
+        const cardImgElement = buttonElement.closest('.cards__card').querySelector('.card__img');
+        cardImgElement.classList.remove('active');
+    }
+        const producto = cart.find(item => item.id === productId);
+        if (producto) {
+            producto.quantity = cantidad
+        }
+    displayCart();
+}
 
 /*Función para eliminar  un producto del carrito*/
 
+function removeFromCart(productId) {
+    cart = cart.filter(item => item.id !== productId);
+
+    const cantidadContainer = document.getElementById(`cantidad-${productId}`).parentElement;
+    const buttonElement = cantidadContainer.previusElementSibling;
+    cantidadContainer.style.display ='none';
+    buttonElement.style.display = 'inline-block';
+
+    const cantidadElemento = document.getElementById(`cantidad-${productId}`);
+    cantidadElemento.textContent = 1;
+
+    const cardImgElement = buttonElement.closest('cards__card').querySelector('.card__img');
+    cardImgElement.classList.remove('active');
+
+    displayCart();
+}
+
 /*Función para mostrar los   productos del carrito*/
+
+function displayCart() {
+    const cartList = document.querySelector('.cart-list__items');
+    const cartHeader = document.querySelector('.cart-list h2');
+    cartList.innerHTML = '';
+
+    let total = 0;
+
+    if (cart.lenght === 0) {
+        cartList.innerHTML = `
+        <div class="items_img">
+            <img src="assets/images/coffee.png" alt="Coffee">
+        </div>
+        <p>Tus bebidas aparecerán aquí</p>
+        `;
+    } else {
+        cart.forEach(item => {
+            total+= item.precio * item.quantity;
+            cartList.innerHTML += `
+            <div class="items__item">
+                <h4>&{item.nombre}</h4>
+            <div class="item__detalles">
+                <p>${item.quantity}x <span>$${item.precio.toFixed(2)}</span></p>
+                <p>$$${(item.precio.toFixed(2) * item.quantity).toFixed(2)}</p>
+                <button onclick="removeFromCart(${item.id})">
+                    <i class="ri-close-circle-line"></i>
+                </button>
+            </div>
+            </div>`
+        })
+    }
+}
 
 /*Función para mostrar el modal del pedido*/
 
